e949615d372e3cd5192e228159536e53e20f079e
Ilan Mitnikov <ilanmitnikov@gmail.com>
2023-02-26 19:34:08 +0000

    molecule initial

---
+++ moleculib/molecule/batch.py
@@ -0,0 +1,176 @@
+from __future__ import annotations
+
+from functools import partial, reduce
+from typing import List, Tuple
+
+import jax.numpy as jnp
+import numpy as np
+from einops import rearrange, repeat
+
+from .alphabet import backbone_atoms
+from .datum import ProteinDatum
+
+
+class ProteinCollator:
+    """
+    Abstract class for collation of multiple ProteinDatum instances
+    into a single batch instance
+    """
+
+    @classmethod
+    def collate(cls, data_list: List[ProteinDatum]):
+        """
+        Takes a list of ProteinDatum instances and produces
+        a ProteinCollate instance with all the instances collated
+        according to the appropriate collate function
+        """
+        raise NotImplementedError("collate method has to be implemented")
+
+    def revert(self) -> List[ProteinDatum]:
+        """
+        Reverts a batch to its original list form
+        """
+        raise NotImplementedError("revert method has to be implemented")
+
+    def to(self, device: str) -> ProteinCollator:
+        for attr, obj in vars(self).items():
+            if type(obj) == np.ndarray:
+                setattr(self, attr, obj.to(device))
+        return self
+
+    def torch(self) -> ProteinCollator:
+        import torch
+
+        for attr, obj in vars(self).items():
+            if type(obj) == np.ndarray:
+                setattr(self, attr, torch.from_numpy(obj))
+        return self
+
+    def to_dict(self, attrs=None):
+        if attrs is None:
+            attrs = vars(self).keys()
+        dict_ = {}
+        for attr in attrs:
+            obj = getattr(self, attr)
+            # strings are not JAX types
+            if type(obj) in [list, tuple]:
+                if type(obj[0]) not in [int, float]:
+                    continue
+                obj = jnp.array(obj)
+            dict_[attr] = obj
+        return dict_
+
+
+class PadBatch(ProteinCollator):
+    def __init__(self, pad_mask, **kwargs):
+        super().__init__()
+        self.pad_mask = pad_mask
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
+
+    def revert(self, constructor=ProteinDatum):
+        data_list = []
+        for batch_index in range(self.pad_mask.shape[0]):
+            mask = self.pad_mask[batch_index]
+            data_attr = dict()
+            for attr, obj in vars(self).items():
+                if attr == "pad_mask":
+                    continue
+                obj = obj[batch_index]
+                if type(obj) == np.ndarray:
+                    obj = obj[mask]
+                data_attr[attr] = obj
+            data_list.append(constructor(**data_attr))
+        return data_list
+
+    @classmethod
+    def collate(cls, data_list):
+        proxy = data_list[0]
+        data_type = type(proxy)
+        unique_type = reduce(lambda res, obj: type(obj) is data_type, data_list, True)
+        assert unique_type, "all data must have same type"
+        max_size = max([len(datum.sequence) for datum in data_list])
+
+        def _maybe_pad_and_stack(obj_list):
+            obj = obj_list[0]
+            if type(obj) != np.ndarray:
+                return obj_list
+            new_list = map(partial(pad_array, total_size=max_size), obj_list)
+            return np.stack(list(new_list), axis=0)
+
+        keys = vars(proxy).keys()
+        assert "bonds_list" not in keys, "PadBatch does not support bonds"
+        value_lists = [vars(datum).values() for datum in data_list]
+        value_lists = zip(*value_lists)
+        values = list(map(_maybe_pad_and_stack, value_lists))
+        batch_attr = dict(zip(keys, values))
+
+        pad_mask = _maybe_pad_and_stack(
+            [np.ones((len(datum.sequence),)) for datum in data_list]
+        ).astype(bool)
+
+        return cls(pad_mask, **batch_attr)
+
+
+class GeometricBatch(ProteinCollator):
+    def __init__(self, batch_index, num_nodes, **kwargs):
+        super().__init__()
+        self.batch_index = batch_index
+        self.num_nodes = num_nodes
+
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
+
+    def revert(self, constructor=ProteinDatum):
+        data_list = []
+        for batch_index in range(len(self.num_nodes)):
+            batch_mask = self.batch_index == batch_index
+            data_attr = dict()
+            for attr, obj in vars(self).items():
+                if attr in ["batch_index", "num_nodes"]:
+                    continue
+                if type(obj) == np.ndarray:
+                    obj = obj[batch_mask]
+                else:
+                    obj = obj[batch_index]
+                data_attr[attr] = obj
+            data_list.append(constructor(**data_attr))
+        return data_list
+
+    @classmethod
+    def collate(cls, data_list):
+        proxy = data_list[0]
+        data_type = type(proxy)
+        unique_type = reduce(lambda _, obj: type(obj) is data_type, data_list, True)
+        assert unique_type, "all data must have same type"
+
+        num_nodes = np.array([len(datum.residue_index) for datum in data_list])
+        nodes_cumsum = np.cumsum([0] + list(num_nodes[:-1]))
+        batch_index = [
+            np.full((size), fill_value=idx) for idx, size in enumerate(num_nodes)
+        ]
+        batch_index = np.concatenate(batch_index, axis=0)
+
+        def maybe_reindex(item):
+            key, obj_list = item
+            if key not in ["bonds_list", "angles_list", "dihedrals_list", "flips_list"]:
+                return obj_list
+            reindex_obj_list = [
+                obj + reindex * 14 for (reindex, obj) in zip(nodes_cumsum, obj_list)
+            ]
+            return reindex_obj_list
+
+        def maybe_concatenate(obj_list):
+            obj = obj_list[0]
+            if type(obj) != np.ndarray:
+                return obj_list
+            return np.concatenate(obj_list, axis=0)
+
+        keys = vars(proxy).keys()
+        value_lists = [vars(datum).values() for datum in data_list]
+        value_lists = zip(*value_lists)
+        value_lists = list(map(maybe_reindex, zip(keys, value_lists)))
+        values = list(map(maybe_concatenate, value_lists))
+        batch_attr = dict(zip(keys, values))
+
+        return cls(batch_index=batch_index, num_nodes=num_nodes, **batch_attr)
---
+++ moleculib/molecule/dataset.py
@@ -0,0 +1,197 @@
+import os
+import pickle
+import traceback
+from functools import partial
+from pathlib import Path
+from tempfile import gettempdir
+from typing import List, Union
+
+import biotite
+import numpy as np
+import pandas as pd
+from pandas import DataFrame, Series
+from torch.utils.data import Dataset
+from tqdm.contrib.concurrent import process_map
+
+from .alphabet import UNK_TOKEN
+from .datum import ProteinDatum
+from .transform import ProteinTransform
+from .utils import pids_file_to_list
+
+MAX_COMPLEX_SIZE = 32
+PDB_METADATA_FIELDS = [
+    ("idcode", str),
+    ("num_res", int),
+    ("standard", bool),
+    ("resolution", float),
+]
+PDB_METADATA_FIELDS += [(f"num_res_{idx}", int) for idx in range(MAX_COMPLEX_SIZE)]
+
+
+class ProteinDataset(Dataset):
+    """
+    Holds ProteinDatum dataset with specified PDB IDs
+
+    Arguments:
+    ----------
+    base_path : str
+        directory to store all PDB files
+    pdb_ids : List[str]
+        list of all protein IDs that should be in the dataset
+    format : str
+        the file format for each PDB file, either "npz" or "pdb"
+    attrs : Union[str, List[str]]
+        a partial list of protein attributes that should be in each protein
+    """
+
+    def __init__(
+        self,
+        base_path: str,
+        transform: ProteinTransform = None,
+        attrs: Union[List[str], str] = "all",
+        metadata: pd.DataFrame = None,
+        max_resolution: float = None,
+        min_sequence_length: int = None,
+        max_sequence_length: int = None,
+        frac: float = 1.0,
+        preload: bool = False,
+        preload_num_workers: int = 10,
+    ):
+
+        super().__init__()
+        self.base_path = Path(base_path)
+        if metadata is None:
+            with open(str(self.base_path / "metadata.pyd"), "rb") as file:
+                metadata = pickle.load(file)
+        self.metadata = metadata
+        self.transform = transform
+
+        if max_resolution is not None:
+            self.metadata = self.metadata[self.metadata["resolution"] <= max_resolution]
+
+        if min_sequence_length is not None:
+            self.metadata = self.metadata[
+                self.metadata["num_res_0"] >= min_sequence_length
+            ]
+
+        if max_sequence_length is not None:
+            self.metadata = self.metadata[
+                self.metadata["num_res_0"] <= max_sequence_length
+            ]
+
+        # shuffle and sample
+        self.metadata = self.metadata.sample(frac=frac).reset_index(drop=True)
+
+        # specific protein attributes
+        protein_attrs = [
+            "idcode",
+            "resolution",
+            "residue_token",
+            "residue_index",
+            "residue_token",
+            "residue_mask",
+            "chain_token",
+            "atom_token",
+            "atom_coord",
+            "atom_mask",
+        ]
+
+        if attrs == "all":
+            self.attrs = protein_attrs
+        else:
+            for attr in attrs:
+                if attr not in protein_attrs:
+                    raise AttributeError(f"attribute {attr} is invalid")
+            self.attrs = attrs
+
+        self.preload = preload
+        if self.preload:
+            proteins = []
+            for idx in range(len(self.metadata.index)):
+                proteins.append(self.load_index(idx))
+            self.proteins = proteins
+
+    def load_index(self, idx):
+        pdb_id = self.metadata.iloc[idx]["idcode"]
+        filepath = os.path.join(self.base_path, f"{pdb_id}.pdb")
+        protein = ProteinDatum.from_filepath(filepath)
+        return protein
+
+    def __len__(self):
+        return len(self.metadata)
+
+    def __getitem__(self, idx):
+        protein = self.proteins[idx] if self.preload else self.load_index(idx)
+        if self.transform is not None:
+            for transformation in self.transform:
+                protein = transformation.transform(protein)
+        return protein
+
+    @staticmethod
+    def _extract_datum_row(datum):
+        is_standard = not (datum.residue_token == UNK_TOKEN).all()
+        metrics = dict(
+            idcode=datum.idcode,
+            standard=is_standard,
+            resolution=datum.resolution,
+            num_res=len(datum.sequence),
+        )
+        for chain in range(np.max(datum.chain_token)):
+            num_residues = (datum.chain_token == chain).sum()
+            metrics[f"num_res_{chain}"] = num_residues
+        return Series(metrics).to_frame().T
+
+    @staticmethod
+    def _maybe_fetch_and_extract(pdb_id, save_path):
+        try:
+            datum = ProteinDatum.fetch_pdb_id(pdb_id, save_path=save_path)
+        except KeyboardInterrupt:
+            exit()
+        except (ValueError, IndexError) as error:
+            print(traceback.format_exc())
+            print(error)
+            return None
+        except (biotite.database.RequestError) as request_error:
+            print(request_error)
+            return None
+        if len(datum.sequence) == 0:
+            return None
+        return ProteinDataset._extract_datum_row(datum)
+
+    @classmethod
+    def build(
+        cls,
+        pdb_ids: List[str] = None,
+        save: bool = True,
+        save_path: str = None,
+        max_workers: int = 1,
+        **kwargs,
+    ):
+        """
+        Builds dataset from scratch given specified pdb_ids, prepares
+        data and metadata for later use.
+        """
+        if pdb_ids is None:
+            root = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+            pdb_ids = pids_file_to_list(root + "/data/pids_all.txt")
+        if save_path is None:
+            save_path = gettempdir()
+
+        series = {c: Series(dtype=t) for (c, t) in PDB_METADATA_FIELDS}
+        metadata = DataFrame(series)
+
+        extractor = partial(cls._maybe_fetch_and_extract, save_path=save_path)
+        if max_workers > 1:
+            rows = process_map(
+                extractor, pdb_ids, max_workers=max_workers, chunksize=50
+            )
+        else:
+            rows = list(map(extractor, pdb_ids))
+        rows = filter(lambda row: row is not None, rows)
+
+        metadata = pd.concat((metadata, *rows), axis=0)
+        if save:
+            with open(str(Path(save_path) / "metadata.pyd"), "wb") as file:
+                pickle.dump(metadata, file)
+
+        return cls(base_path=save_path, metadata=metadata, **kwargs)
