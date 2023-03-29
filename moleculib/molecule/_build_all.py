f292479d7c2eced86f913fc68b0122196e954906
Ilan Mitnikov <ilanmitnikov@gmail.com>
2023-03-29 16:58:55 +0000

    test

--- moleculib/molecule/_build_all.py
+++ moleculib/molecule/_build_all.py
@@ -10,3 +10,15 @@ a = ds.metadata.atom_count.hist(bins=1022)
 count, division = np.histogram(ds.metadata.atom_count, bins=1023)
 print("Unique res: ", ds.metadata.res_name.nunique())
 print("Unique res id: ", ds.metadata.res_id.nunique())
+
+print(ds.metadata[ds.metadata.res_name=="SPM"].atom_count.unique())
+
+filter = (ds.metadata.res_name=="SPM") & (ds.metadata.atom_count==20)
+print(np.sum(filter))
+print(ds.metadata[filter].idcode)
+
+fil2 = ds.metadata.idcode == "1DPL"
+print(ds.metadata[fil2])
+
+fil2 = ds.metadata.idcode == "1M69"
+print(ds.metadata[fil2])
