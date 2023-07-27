e215fbe8ba53c8a0a62de1f3151d2223bb0da7a9
dana <danaru@mit.edu>
2023-07-27 15:16:40 +0000

    notebook with irreps and plot

--- moleculib/nucleic/datum.py
+++ moleculib/nucleic/datum.py
@@ -16,18 +16,155 @@ from biotite.structure import filter_nucleotides
 import os
 import biotite.structure.io.mmtf as mmtf
 
-from alphabet import (
-    all_atoms,
-    all_nucs,
-    backbone_atoms_DNA,
-    backbone_atoms_RNA,
-    atom_index,
-    atom_to_nucs_index,
-    get_nucleotide_index,
-    MAX_DNA_ATOMS
+import sys
+sys.path.append('.')
+
+# from alphabet import (
+#     all_atoms,
+#     all_nucs,
+#     backbone_atoms_DNA,
+#     backbone_atoms_RNA,
+#     atom_index,
+#     atom_to_nucs_index,
+#     get_nucleotide_index,
+#     MAX_DNA_ATOMS
+# )
+
+## alphabet:
+from collections import OrderedDict
+from typing import List
+
+import numpy as np
+from ordered_set import OrderedSet
+
+UNK_TOKEN = 1
+#5 carbon atoms 8 hydrogen atoms 1 nitrogen atom (in the base) 
+# 1 phosphorus atom (in the phosphate group) 4 oxygen atoms (in the sugar and phosphate group)
+MAX_RES_ATOMS = 14
+# from https://x3dna.org/articles/name-of-base-atoms-in-pdb-formats#:~:text=Canonical%20bases%20(A%2C%20C%2C,%2C%20C8%2C%20N9)%20respectively.
+
+#ensure order and keep track of what atoms are present or missing
+base_atoms_per_nuc = OrderedDict(
+        A=["N1","C2","N3", "C4", "C5", "C6", "N6", "N7", "C8", "N9"], # GREEN
+        U=["N1", "C2", "O2", "N3", "C4", "O4", "C5", "C6"], #RED
+        RT=["N1", "C2", "O2", "N3", "C4", "O4", "C5", "C5M", "C6"], #Calman Didn't have C5M  'C5' instead
+        G=["N1", "C2", "N2", "N3", "C4", "C5", "C6", "O6", "N7", "C8", "N9"], #light pink (has N9, O6)
+        C=["N1", "C2", "O2", "N3", "C4", "N4", "C5", "C6"], # pink (has o2)
+        I = [], #from gpt: I=["N1", "C2", "N3", "C4", "C5", "C6"]
+        #NOTE: in the file where I took the atoms, DA's atoms are N1A, C2A, etc.
+        #but from working with the data I saw that they're regular without 'A'
+        DA = ["N1","C2", "N3", "C4", "C5", "N6", "C6", "N7","C8","N9"],#olive GREEN
+        DC =["N1", "C2", "O2", "N3", "C4", "N4", "C5", "C6"], #tourcize
+        DG =["N1", "C2", "N2", "N3", "C4", "C5", "C6", "O6", "N7", "C8", "N9"], #light blue-violet
+        DI =[],
+        #NOTE: actually didn't see C5M in the data itself but leave it in case it will come up
+        DT = ["N1", "C2", "O2", "N3", "C4", "O4", "C5", "C5M", "C6"], #purple
+        DU =["N1", "C2", "O2", "N3", "C4", "O4", "C5", "C6"] #mutagenic U (doesn't suppose to be a DNA nuc but RNA nuc)
 )
 
-from utils import  pdb_to_atom_array
+backbone_atoms_DNA = ["C1'", "C2'", "C3'", "C4'", "C5'","P", "O1P", "O2P","O3P", "O3'", "O4'", "O5'"] #NOTE: add "" for carbons and Oxygens as in the file
+backbone_atoms_RNA = ["C1'", "C2'", "C3'", "C4'", "C5'","P", "O1P", "O2P","O3P","O2'", "O3'", "O4'", "O5'"]  
+
+### TODO: Should it be the same as below (from alphabet protein)?
+# backbone_chemistry = dict(
+#     bonds=[["N", "CA"], ["CA", "C"], ["C", "O"]],
+#     angles=[["CA", "C", "O"], ["N", "CA", "C"]],
+#     dihedrals=[["N", "CA", "C", "O"]],
+#     flippable=[],
+# )
+special_tokens = ["PAD", "UNK"] #what do whese represent?
+
+atoms_per_nuc = OrderedDict()
+atoms_per_nuc["PAD"] = []
+atoms_per_nuc["UNK"] = [] # backbone_atoms
+##TODO check the DUPLICATES situation
+#for every nuc we add the base and backbone atoms
+for nuc, base_atoms in base_atoms_per_nuc.items():
+    if nuc in ['A', 'U', 'RT', 'G', 'C', 'I']: #RNA
+        atoms_per_nuc[nuc] = base_atoms + backbone_atoms_RNA
+    else:
+        atoms_per_nuc[nuc] = base_atoms + backbone_atoms_DNA
+MAX_DNA_ATOMS = max([len(atoms) for atoms in atoms_per_nuc.values()])###==24
+
+all_atoms = list(OrderedSet(sum(list(atoms_per_nuc.values()), [])))
+all_atoms = special_tokens + all_atoms
+all_atoms_tokens = np.arange(len(all_atoms))
+
+elements = list(OrderedSet([atom[0] for atom in all_atoms]))
+all_atoms_elements = np.array([elements.index(atom[0]) for atom in all_atoms])
+# all_atoms_radii = np.array(
+#     [
+#         (van_der_walls_radii[atom[0]] if (atom[0] in van_der_walls_radii) else 0.0)
+#         for atom in all_atoms
+#     ]
+# )
+
+all_nucs = list(base_atoms_per_nuc.keys())
+all_nucs = special_tokens + all_nucs
+all_nucs_tokens = np.arange(len(all_nucs))
+all_nucs_atom_mask = np.array(
+    [
+        ([1] * len(atoms) + [0] * (MAX_DNA_ATOMS - len(atoms)))
+        for (_, atoms) in atoms_per_nuc.items()
+    ]
+).astype(np.bool_)
+
+def _atom_to_all_nucs_index(atom):
+    def _atom_to_nuc_index(nuc):
+        nuc_atoms = atoms_per_nuc[nuc]
+        mask = atom in nuc_atoms
+        index = nuc_atoms.index(atom) if mask else 0
+        return index, mask
+
+    indices, masks = zip(*list(map(_atom_to_nuc_index, all_nucs)))
+    return np.array(indices), np.array(masks)
+
+
+def _index(lst: List[str], item: str) -> int:
+    try:
+        index = lst.index(item)
+    except ValueError:
+        index = UNK_TOKEN  # UNK
+    return index
+
+
+def atom_index(atom: str) -> int:
+    return _index(all_atoms, atom)
+
+def get_nucleotide_index(nucleotide: str) -> int:
+    return _index(all_nucs, nucleotide)
+
+atom_to_nucs_index, atom_to_nucs_mask = zip(
+    *list(map(_atom_to_all_nucs_index, all_atoms))
+)
+atom_to_nucs_index = np.array(atom_to_nucs_index)
+atom_to_nucs_mask = np.array(atom_to_nucs_mask)
+
+#### end of alphabet
+
+# from utils import  pdb_to_atom_array
+import os
+from pathlib import Path
+
+from biotite.structure import filter_nucleotides
+from biotite.structure.io.pdb import PDBFile
+
+
+import numpy as np
+
+home_dir = str(Path.home()) #not sure what this do
+config = {"cache_dir": os.path.join(home_dir, ".cache", "moleculib")} #not sure either
+
+
+def pdb_to_atom_array(pdb_path):
+    pdb_file = PDBFile.read(pdb_path)
+    atom_array = pdb_file.get_structure(
+        model=1, extra_fields=["atom_id", "b_factor", "occupancy", "charge"])
+    aa_filter = filter_nucleotides(atom_array)
+    atom_array = atom_array[aa_filter]
+    return atom_array
+
+##END OF UTILS
 
 class NucleicDatum:
     """
@@ -61,7 +198,7 @@ class NucleicDatum:
     
 
     def __len__(self):
-        return len(self.sequence)
+        return len(self.nuc_index)
 
     @classmethod
     def _extract_reshaped_atom_attr(
@@ -312,13 +449,15 @@ def _scatter_coord(name, coord, color='black', visible=True):
     
     return fig
 
+# np.array(list(map(lambda atom: atom_index(atom), atom_array.atom_name)))
 rna_res_names = ['A', 'U', 'RT', 'G', 'C', 'I']
 dna_res_names = ['DA', 'DU', 'DT', 'DG', 'DC', 'DI']
 dna_res_tokens = list(map(lambda res: get_nucleotide_index(res), dna_res_names))
 rna_res_tokens = list(map(lambda res: get_nucleotide_index(res), rna_res_names))
 
+
 if __name__ == '__main__':
-    dna_datum = NucleicDatum.fetch_pdb_id('2QK9')#('5F9R')    
+    dna_datum = NucleicDatum.fetch_pdb_id('5F9R')    
     # breakpoint()
     ##DNADATUM: str,
         # resolution: float,
@@ -396,6 +535,10 @@ if __name__ == '__main__':
     dna_res_names = ['DA', 'DU', 'DT', 'DG', 'DC', 'DI']
     dna_res_tokens = list(map(lambda res: get_nucleotide_index(res), dna_res_names))
     rna_res_tokens = list(map(lambda res: get_nucleotide_index(res), rna_res_names))
+
+    rna_res_tokens_dict = {res: get_nucleotide_index(res) for res in rna_res_names}
+    dna_res_tokens_dict = {res: get_nucleotide_index(res) for res in dna_res_names}
+
     
     num_chains = dna_datum.chain_token[-1]
     chains_len = get_repetition_lengths(dna_datum.chain_token)
@@ -409,6 +552,7 @@ if __name__ == '__main__':
             follow.append('confused')
     print(dna_datum.nuc_token[140])
     print(len(dna_datum))
+    print(len(dna_datum.nuc_token))
     #number of RNA chains, 
     # for chain in range(num_chains+1):
     #     chain_len = chains_len[chain]
@@ -418,7 +562,7 @@ if __name__ == '__main__':
     # how long the chain is
     #total number of nucleotides, 
     
-    # breakpoint()
+    breakpoint()
     
 
 
