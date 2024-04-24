e05187afc1e2c6e34ad492f691d72e9cf053b9ba
danaru <danaru@mit.edu>
2024-04-24 02:12:22 +0000

    fixed datum O1P--> OP1 etc

--- moleculib/nucleic/datum.py
+++ moleculib/nucleic/datum.py
@@ -18,139 +18,15 @@ import biotite.structure.io.mmtf as mmtf
 from einops import rearrange, repeat
 
 
+import plotly.graph_objects as go
+import plotly.offline as pyo
+
 import sys
 sys.path.append('.')
 
-# from alphabet import (
-#     all_atoms,
-#     all_nucs,
-#     backbone_atoms_DNA,
-#     backbone_atoms_RNA,
-#     atom_index,
-#     atom_to_nucs_index,
-#     get_nucleotide_index,
-#     MAX_DNA_ATOMS
-# )
-
-## alphabet:
-from collections import OrderedDict
-from typing import List
-
-import numpy as np
-from ordered_set import OrderedSet
-
-UNK_TOKEN = 1
-#5 carbon atoms 8 hydrogen atoms 1 nitrogen atom (in the base) 
-# 1 phosphorus atom (in the phosphate group) 4 oxygen atoms (in the sugar and phosphate group)
-MAX_RES_ATOMS = 14
-# from https://x3dna.org/articles/name-of-base-atoms-in-pdb-formats#:~:text=Canonical%20bases%20(A%2C%20C%2C,%2C%20C8%2C%20N9)%20respectively.
-
-#ensure order and keep track of what atoms are present or missing
-base_atoms_per_nuc = OrderedDict(
-        A=["N1","C2","N3", "C4", "C5", "C6", "N6", "N7", "C8", "N9"], # GREEN
-        U=["N1", "C2", "O2", "N3", "C4", "O4", "C5", "C6"], #RED
-        RT=["N1", "C2", "O2", "N3", "C4", "O4", "C5", "C5M", "C6"], #Calman Didn't have C5M  'C5' instead
-        G=["N1", "C2", "N2", "N3", "C4", "C5", "C6", "O6", "N7", "C8", "N9"], #light pink (has N9, O6)
-        C=["N1", "C2", "O2", "N3", "C4", "N4", "C5", "C6"], # pink (has o2)
-        I = [], #from gpt: I=["N1", "C2", "N3", "C4", "C5", "C6"]
-        #NOTE: in the file where I took the atoms, DA's atoms are N1A, C2A, etc.
-        #but from working with the data I saw that they're regular without 'A'
-        DA = ["N1","C2", "N3", "C4", "C5", "N6", "C6", "N7","C8","N9"],#olive GREEN
-        DC =["N1", "C2", "O2", "N3", "C4", "N4", "C5", "C6"], #tourcize
-        DG =["N1", "C2", "N2", "N3", "C4", "C5", "C6", "O6", "N7", "C8", "N9"], #light blue-violet
-        DI =[],
-        #NOTE: actually didn't see C5M in the data itself but leave it in case it will come up
-        DT = ["N1", "C2", "O2", "N3", "C4", "O4", "C5", "C5M", "C6"], #purple
-        DU =["N1", "C2", "O2", "N3", "C4", "O4", "C5", "C6"] #mutagenic U (doesn't suppose to be a DNA nuc but RNA nuc)
-)
-
-backbone_atoms_DNA = ["C1'", "C2'", "C3'", "C4'", "C5'","P", "O1P", "O2P","O3P", "O3'", "O4'", "O5'"] #NOTE: add "" for carbons and Oxygens as in the file
-backbone_atoms_RNA = ["C1'", "C2'", "C3'", "C4'", "C5'","P", "O1P", "O2P","O3P","O2'", "O3'", "O4'", "O5'"]  
-
-### TODO: Should it be the same as below (from alphabet protein)?
-# backbone_chemistry = dict(
-#     bonds=[["N", "CA"], ["CA", "C"], ["C", "O"]],
-#     angles=[["CA", "C", "O"], ["N", "CA", "C"]],
-#     dihedrals=[["N", "CA", "C", "O"]],
-#     flippable=[],
-# )
-special_tokens = ["PAD", "UNK"] #what do whese represent?
-
-atoms_per_nuc = OrderedDict()
-atoms_per_nuc["PAD"] = []
-atoms_per_nuc["UNK"] = [] # backbone_atoms
-##TODO check the DUPLICATES situation
-#for every nuc we add the base and backbone atoms
-for nuc, base_atoms in base_atoms_per_nuc.items():
-    if nuc in ['A', 'U', 'RT', 'G', 'C', 'I']: #RNA
-        atoms_per_nuc[nuc] = base_atoms + backbone_atoms_RNA
-    else:
-        atoms_per_nuc[nuc] = base_atoms + backbone_atoms_DNA
-MAX_DNA_ATOMS = max([len(atoms) for atoms in atoms_per_nuc.values()])###==24
-
-all_atoms = list(OrderedSet(sum(list(atoms_per_nuc.values()), [])))
-all_atoms = special_tokens + all_atoms
-all_atoms_tokens = np.arange(len(all_atoms))
-
-elements = list(OrderedSet([atom[0] for atom in all_atoms]))
-all_atoms_elements = np.array([elements.index(atom[0]) for atom in all_atoms])
-# all_atoms_radii = np.array(
-#     [
-#         (van_der_walls_radii[atom[0]] if (atom[0] in van_der_walls_radii) else 0.0)
-#         for atom in all_atoms
-#     ]
-# )
-
-all_nucs = list(base_atoms_per_nuc.keys())
-all_nucs = special_tokens + all_nucs
-all_nucs_tokens = np.arange(len(all_nucs))
-all_nucs_atom_mask = np.array(
-    [
-        ([1] * len(atoms) + [0] * (MAX_DNA_ATOMS - len(atoms)))
-        for (_, atoms) in atoms_per_nuc.items()
-    ]
-).astype(np.bool_)
-
-all_nucs_atom_tokens = np.array(
-    [
-        ([all_atoms.index(atom) for atom in atoms] + [0] * (24 - len(atoms)))
-        for (_, atoms) in atoms_per_nuc.items()
-    ]
-)
+from .alphabet import *
 
 
-def _atom_to_all_nucs_index(atom):
-    def _atom_to_nuc_index(nuc):
-        nuc_atoms = atoms_per_nuc[nuc]
-        mask = atom in nuc_atoms
-        index = nuc_atoms.index(atom) if mask else 0
-        return index, mask
-
-    indices, masks = zip(*list(map(_atom_to_nuc_index, all_nucs)))
-    return np.array(indices), np.array(masks)
-
-
-def _index(lst: List[str], item: str) -> int:
-    try:
-        index = lst.index(item)
-    except ValueError:
-        index = UNK_TOKEN  # UNK
-    return index
-
-
-def atom_index(atom: str) -> int:
-    return _index(all_atoms, atom)
-
-def get_nucleotide_index(nucleotide: str) -> int:
-    return _index(all_nucs, nucleotide)
-
-atom_to_nucs_index, atom_to_nucs_mask = zip(
-    *list(map(_atom_to_all_nucs_index, all_atoms))
-)
-atom_to_nucs_index = np.array(atom_to_nucs_index)
-atom_to_nucs_mask = np.array(atom_to_nucs_mask)
-
-#### end of alphabet
 
 # from utils import  pdb_to_atom_array
 import os
@@ -452,7 +328,7 @@ class NucleicDatum:
             line[6:11] = str(idx + 1).ljust(5)
             line[12:16] = name.ljust(4)
             line[17:20] = res_name.ljust(3)
-            line[21:22] = "A"
+            line[21:22] = "A" ##if that is chain identifier it should go from a to z?
             line[23:27] = str(res_index + 1).ljust(4)
             line[30:38] = f"{x:.3f}".rjust(8)
             line[38:46] = f"{y:.3f}".rjust(8)
@@ -474,7 +350,6 @@ class NucleicDatum:
     ):
         if viewer is None:
             viewer = (0, 0)
-        print(self.to_pdb_str())
         view.addModel(self.to_pdb_str(), 'pdb', viewer=viewer)
         view.setStyle({'model': -1}, {}, viewer=viewer)
         if sphere:
@@ -493,9 +368,6 @@ class NucleicDatum:
 
 
 
-
-import plotly.graph_objects as go
-import plotly.offline as pyo
 def _scatter_coord(name, coord, color='black', visible=True):
     sc_coords = []
     x, y, z = coord.T
