1f530c9cce518c4035d66abcc31a282f1a346d08
Ilan Mitnikov <ilanmitnikov@gmail.com>
2024-05-16 22:25:15 +0000

    mddensity ds fix

--- moleculib/molecule/dataset.py
+++ moleculib/molecule/dataset.py
@@ -1444,16 +1444,17 @@ class MDDensityDataset(Dataset):
         self.n_grid = 50  # number of grid points along each dimension
         self.grid_size = 20.  # box size in Bohr
         self.data_path = os.path.join(self.root, mol_name, f'{mol_name}_{self.split}')
-
+        self.samples = samples
         self.atom_type = ATOM_TYPES[mol_name]
         self.atom_coords = np.load(os.path.join(self.data_path, 'structures.npy'))
         self.densities = self._convert_fft(np.load(os.path.join(self.data_path, 'dft_densities.npy')))
         self.grid_coord = self._generate_grid()
-        print("Loaded {} {} datapoints".format(mol_name, self.split))
+        print("Loaded {} {} {} datapoints".format(mol_name, self.split, len(self)))
         if _split == "train":
             self.splits = {"train": RotatingPoolData(self, 300) if _rotated else self}
             test = self.__class__(
-                samples=5000,
+                mol_name,
+                samples=samples*5,
                 _split="test",
             )
             self.splits["test"] = RotatingPoolData(test, 90) if _rotated else test
@@ -1462,25 +1463,25 @@ class MDDensityDataset(Dataset):
         # The raw data are stored in Fourier basis, we need to convert them back.
         print(f'Precomputing {self.split} density from FFT coefficients ...')
         fft_coeff = np.array(fft_coeff).astype(np.complex64)
-        d = fft_coeff.view(-1, self.n_grid, self.n_grid, self.n_grid)
+        d = fft_coeff.reshape(-1, self.n_grid, self.n_grid, self.n_grid)
         hf = self.n_grid // 2
         # first dimension
         d[:, :hf] = (d[:, :hf] - d[:, hf:] * 1j) / 2
         d[:, hf:] = np.flip(d[:, 1:hf + 1], [1]).conj()
-        d = np.fft.ifft(d, dim=1)
+        d = np.fft.ifft(d, axis=1)
         # second dimension
         d[:, :, :hf] = (d[:, :, :hf] - d[:, :, hf:] * 1j) / 2
         d[:, :, hf:] = np.flip(d[:, :, 1:hf + 1], [2]).conj()
-        d = np.fft.ifft(d, dim=2)
+        d = np.fft.ifft(d, axis=2)
         # third dimension
         d[..., :hf] = (d[..., :hf] - d[..., hf:] * 1j) / 2
         d[..., hf:] = np.flip(d[..., 1:hf + 1], [3]).conj()
-        d = np.fft.ifft(d, dim=3)
+        d = np.fft.ifft(d, axis=3)
         return np.flip(d.real.reshape(-1, self.n_grid ** 3), [-1])
 
     def _generate_grid(self):
         x = np.linspace(self.grid_size / self.n_grid, self.grid_size, self.n_grid)
-        return np.stack(np.meshgrid(x, x, x, indexing='ij'), dim=-1).reshape(-1, 3)
+        return np.stack(np.meshgrid(x, x, x, indexing='ij'), axis=-1).reshape(-1, 3)
 
     def __getitem__(self, item):
         grid = self.grid_coord
